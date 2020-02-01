51bc1fddc98824f263b4844c6dab7c18fb636230
Murtaza <Murtaza_taloo@hotmail.com>
2020-02-02 08:06:15 +1100

    added a 'fulfilled' Boolean field in the order schema

--- models/Order.js
+++ models/Order.js
@@ -7,6 +7,7 @@ const orderSchema = new Schema(
     items: Array, // this will contain one or more listing objects
     totalValue: Number,
     numberOfItems: Number,
+    fulfilled: Boolean,
     customer: Object // array of objects or just object
   },
   { timestamps: true },
