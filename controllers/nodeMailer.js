e9f37695275c0a2c1dad448cb23b1b17f9b389ec
Murtaza <Murtaza_taloo@hotmail.com>
2020-02-06 02:18:23 +1100

    now a message goes through on the success email sent

--- controllers/nodeMailer.js
+++ controllers/nodeMailer.js
@@ -2,11 +2,10 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const sendMail = async (req, res) => {
-
-    console.log(req.body)
-    console.log(req.body.name)
-    nodemailer.createTestAccount((err, account) => {
-      const htmlEmail = `
+  console.log(req.body);
+  console.log(req.body.name);
+  nodemailer.createTestAccount((err, account) => {
+    const htmlEmail = `
         <h3>Contact Details</h3>
         <ul>
           <li> Name: ${req.body.name}</li>
@@ -14,32 +13,34 @@ const sendMail = async (req, res) => {
         </ul>
         <h3>Message</h3>
         <p>${req.body.message}</p>
-      `
-      let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        requireTLS: true,
-        auth: {
-          user: process.env.EMAIL,
-          pass: process.env.PASSWORD
-        }
-      })
-      let mailOptions = {
-        from: 'xmedicinepower@gmail.com',
-        to: 'xmedicinepower@gmail.com',
-        subject: 'New Message',
-        text: req.body.message,
-        html: htmlEmail
+      `;
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      requireTLS: true,
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
       }
-      transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-          console.log('Error', err );
-        } else {
-          console.log('Email En route!');
-        }
-      })
-    })
-  }
-  
+    });
+    let mailOptions = {
+      from: "xmedicinepower@gmail.com",
+      to: "xmedicinepower@gmail.com",
+      subject: "New Message",
+      text: req.body.message,
+      html: htmlEmail
+    };
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log(res);
+        res.status(200).send("Your Email has been successfully sent");
+        console.log("Email En route!");
+      }
+    });
+  });
+};
+
 module.exports = sendMail;
